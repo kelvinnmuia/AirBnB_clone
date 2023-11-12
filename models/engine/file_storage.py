b89ff0d8043c38f0bb6208710867cdcf9a404c6c
kelvinnmuia <kelvinnmuia@gmail.com>
2023-11-12 11:40:23 -0800

    refactored the console code to fix bugs

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-defines the FileStorage class
+Defines the FileStorage class
 """
 
 from json import loads, dumps
@@ -8,35 +8,34 @@ from os.path import exists
 
 
 class FileStorage():
-    """
-    serializes instances to a JSON file and
-    deserializes JSON file to instances
+    """ serializes to / deserializes from a JSON file
 
-    Attributes:
-        __file_path (str): the path to the JSON file
-        __objects (dict): will help store objects by class.id
+        Attributes:
+            __file_path (str): path to JSON file
+            __objects (dict): will store objects by class.id
     """
-    __file_path = "file.json"
-    __objects = {}
+    __file_path = "file.json"  # string - path to JSON file
+    __objects = {}  # dictionary - empty but will store objects by class.id
 
     def all(self):
-        """
-        returns the dictionary of all saved objects
-        """
+        """ returns dictionary of all saved objects """
         return self.__objects
 
     def new(self, obj):
         """
-         sets in __objects the obj with key <obj class name>.id
+        stores obj in __objects dictionary:
+                key = "<obj class name>.id"
+                value = obj.to_dict()
+
+            Args:
+                obj (:obj:`BaseModel`): object to add to __objects
         """
         key = "{}.{}".format(type(obj).__name__, obj.id)
         self.__objects[key] = obj
         self.save()
 
     def save(self):
-        """
-        serializes __object to the JSON file
-        """
+        """ serializes __object to the JSON file """
         dict_of_dicts = {}
         for key, value in self.__objects.items():
             dict_of_dicts[key] = value.to_dict()
@@ -44,11 +43,10 @@ class FileStorage():
             f.write(dumps(dict_of_dicts))
 
     def reload(self):
-        """
-        deserializes JSON file to __objects
+        """ deserializes JSON file to __objects
 
-        NB:
-            nothing is done when JSON file (__file_path) does not exist
+        Note:
+            if JSON file (__file_path) does not exist, nothing is done.
         """
 
         if exists(self.__file_path) is False:
